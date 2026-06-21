18f34985e33d110218f748e31165c61ccf1e32b1
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-06-21 16:34:40 +0500

    js variable&dataType

---
+++ javaScript/variableDT.js
@@ -0,0 +1,42 @@
+// console.log('uzair');
+// console.log("welcome to js");
+
+//variables let,const,var
+//let a=10
+ //let a=200;     // error
+//  var b=35;
+//  var b=400;   // old method not error but we cannot use this
+let name="uzair";
+const cgpa=3.8;   
+//cgpa=4    cannot change constant value
+let isStudent=true;
+let isTeacher=false;
+console.log(name);
+console.log(cgpa);
+console.log(isStudent);
+//console.log(name,cgpa,isStudent,isTeacher)
+
+//Data Types Primitive(string,number,boolean,undefiend,Null,Bigint,symble) nonprimitive(object,arrays,function)
+
+//number
+let age=19;
+let price=1300;
+console.log(typeof age);
+
+//string
+let city="lodhran";
+let university="iub";
+console.log(typeof city,typeof university);
+
+//boolean
+let isMale=true;
+let isFemale=false;
+console.log(typeof isMale);
+
+//undefined
+let x;
+console.log(typeof x);
+
+//null
+let y=null;
+console.log(typeof y);
