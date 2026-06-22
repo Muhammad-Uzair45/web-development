e1a9c0de1b78c20f2ffcfd9540992cde9a5a5b54
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-06-22 16:31:32 +0500

    Operaters practice

---
+++ javaScript/operaters/operater.js
@@ -0,0 +1,66 @@
+                        // Operaters
+// Arithmetic operaters  (+ ,  - , * , / , % , exponentional, inCrement, deCrement)
+let a = 8;
+let b = 7;
+console.log("8+7=",a+b);  // 15
+console.log("8-7=",a-b);  // 1
+console.log("8*7=",a*b);  // 56
+console.log("8/7=",a/b);  // 1.14
+console.log("8%7=",a%b);   // 1
+console.log("8**7=",8**7)   //2,097,152     exponentional
+
+/*     // inCrement & deCrement( Unary operaters)
+let q =3
+let p =8
+let n = 5;
+n++;     //  n=n+1
+console.log(n); 
+console.log(p++);   //output 8  because this is post_increment change result in next line
+console.log(p);      // now change 9
+console.log(++q);   // output 4 pre_increment first increment then print
+
+//deCrement
+let t =19;
+let r =14;
+//r--;
+console.log(r--);  // 14    decrement apply in next line
+console.log(r);  //   now  13
+console.log(--t) // 18 output                  */ 
+
+// Assignment operater (= , += , -= , *=, **=  , %=)
+/*
+ let a = 3;
+ let b = 5;
+ console.log(a=b);  //5
+ console.log(a+=20);
+ console.log(a-=b);
+ console.log(a*=b);
+console.log(a**b);
+ console.log(b%=a);                 */
+
+/*    // Comparison operaters (== ,  ===  ,  != , !===,  > ,  < ,  >= , <= ,)
+
+let c = 6;
+let d = 2;
+let e = 6;
+let f = "6"
+console.log(c==d);   // false
+console.log(c == e)  // true
+console.log(c==f);   //  ture   this is compare only value
+console.log(c===f);   //  false   this is compare both value and type
+console.log(c != d);   //  true
+console.log(c != e);   // false
+console.log(c > d);  // true
+console.log(c >= d);   // true
+console.log(c < d);   // false
+console.log(c <= d);   // false    */ 
+
+ /*    // Logical operaters (&&  , ||  , !)
+let a = 4;
+let b = 6;
+console.log( a!==b && a<b );  // true 
+console.log(a===b  &&  a<b);   // false
+console.log(a===b  || a<b);  // true
+console.log(a===b  || a>b);   // false
+console.log(  !(a<b) );    // false
+console.log(  !(a>b) );    // true               */
