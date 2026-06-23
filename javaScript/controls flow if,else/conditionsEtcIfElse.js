659d9cb9c76c713f15ef8aed25615a87e095a930
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-06-23 20:18:58 +0500

    conditional statements add

---
+++ javaScript/controls flow if,else/conditionsEtcIfElse.js
@@ -0,0 +1,60 @@
+   // controls flow Like  Conditional statements(if, if-else ,  else-if ,switch) loops
+   // if  conditional statement
+   /*
+   let num = 83;        
+   if (num >= 33) {
+    console.log("Pass");  //if condition is true show output other skip
+   }          
+       */
+   // if-else  statement
+  /*
+   let age =15;             //if condition is false then esle part run
+   if (age>=18) {
+    console.log("Eligible for vote");
+   } else {
+    console.log("Not Eligible for vote");
+   }              */
+   // else-if statement
+  /*
+   let marks = 85
+   if (marks >=90 ) {
+    console.log("Grade: A+");
+   } else if (marks >=80) {
+    console.log("Grade: A");
+   } else if (marks >=70) {
+    console.log("Grade: B");
+   } else if (marks >=60) {
+    console.log("Grade: C");
+   } else if (marks >=50){  
+      console.log("Grade: D");
+   } else {
+    console.log("Fail");
+   }            */
+  
+   /*  // Ternary operater   
+   //syntax: condition? true output: false output
+   
+   let age =37;
+    age >= 18 ? console.log("can vote"): console.log("cannot vote" );
+            */
+   // switch statement
+   
+   /*
+   let day = 4;
+   switch (day) {
+      case 1:
+         console.log("Monday");
+         break;
+      case 2:
+         console.log("Tuesday");
+         break;
+      case 3:
+         console.log("Wenesday");
+         break;
+      case 4:
+         console.log("Thursday");
+         break;
+      default:
+         console.log("Invalid");          
+
+   }   */ 
