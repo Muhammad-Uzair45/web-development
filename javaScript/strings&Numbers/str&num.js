2ee6c15a82cc919f8ac020b4d692992d0685ae8a
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-06-24 23:07:37 +0500

    strings add

---
+++ javaScript/strings&Numbers/str&num.js
@@ -0,0 +1,32 @@
+// 3 way of creating strings
+/*
+const str1 = "Uzair";
+const str2 = 'learning';
+const str3 = `javaScript`;     //modren way
+console.log(str1);
+console.log(str2);
+console.log(str3); 
+//console.log(`I'm ${str1}  from Pakistan ${str2} ${str3}`); 
+   */ 
+
+const str = `My name is Uzair`;
+//console.log(str.length);
+//console.log(str.toUpperCase());
+//console.log(str.toLowerCase());
+//console.log(str[4]);    
+//console.log(str.trim());    // remove starting and ending white spacing
+//console.log(str.slice(4,9));
+//console.log(str.charAt(3));
+//console.log(str.includes("m"));
+//console.log(str.indexOf("m"));
+//console.log(str.replace("z" , "m"));    // replaceAll replace all select items
+
+
+
+
+// Template litral    use for concate
+const name = "Uzair";
+const age = 19;
+console.log(`My name is ${name} and ${age} years old`);
+
+//console.log(str.concat(age));           
