45f519d016d82a4906bd9ea11219f2fbe9473736
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-06-24 16:56:45 +0500

    conditional statement and loops

--- javaScript/controls flow if,else/conditionsEtcIfElse.js
+++ javaScript/controls flow if,else/conditionsEtcIfElse.js
@@ -58,3 +58,33 @@
          console.log("Invalid");          
 
    }   */ 
+  // Loops (for , while , do while, for-in ,for-of )
+      //for loop
+/*
+      for (let i=1; i<=6; i++ ){         
+         console.log("Pakistan");      //print six times Pakistan
+      }                */
+     //calculate the the sum of first 8 number?
+ /*    let sum =0;
+     for (let i=1; i<=8; i++){
+        sum=sum + i;
+        console.log(sum);
+     }                               */
+
+   //while loop
+/*
+   let i = 1;
+   while (i <=4){
+      console.log("Uzair");  // print four times Uzair
+      i++;
+   }            */
+  // do while loop at least one time run
+ /*
+  let i = 11;
+  do{
+   console.log(i);
+   i++;
+  }while (i<=5);   */   
+
+
+
