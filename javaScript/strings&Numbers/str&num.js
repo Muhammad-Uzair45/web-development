d9f05a12b0f0f8aa61cca250f145df3b65162a97
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-06-28 07:35:09 +0500

    number&math and Date add

--- javaScript/strings&Numbers/str&num.js
+++ javaScript/strings&Numbers/str&num.js
@@ -9,7 +9,7 @@ console.log(str3);
 //console.log(`I'm ${str1}  from Pakistan ${str2} ${str3}`); 
    */ 
 
-const str = `My name is Uzair`;
+//const str = `My name is Uzair`;
 //console.log(str.length);
 //console.log(str.toUpperCase());
 //console.log(str.toLowerCase());
@@ -25,8 +25,45 @@ const str = `My name is Uzair`;
 
 
 // Template litral    use for concate
-const name = "Uzair";
-const age = 19;
-console.log(`My name is ${name} and ${age} years old`);
 
-//console.log(str.concat(age));           
+// const str = `My name is Uzair`;
+// const name = "Uzair";
+// const age = 19;
+// console.log(`My name is ${name} and ${age} years old`);
+
+//console.log(str.concat(age));   
+
+// numbers and Math
+let a = 8;
+let b = 23.1478;
+console.log(a, typeof(a));
+console.log(b , typeof(b));
+console.log(b.toFixed(2));
+console.log(b.toPrecision(5));
+console.log(a.toString());
+ //math
+console.log(Math.PI);
+console.log(Math.sqrt(16));
+console.log(Math.ceil(4.15));
+console.log(Math.floor(4.85));
+console.log(Math.round(6.59));
+console.log(Math.pow(2,3));
+console.log(Math.trunc(34.564));
+console.log(Math.max(10,15,18,25,12));
+console.log(Math.min(6,5,13,10,));
+console.log(Math.random()*10+1);
+
+// date
+// let today = new Date();
+// console.log(today);
+
+// let birthday = new Date("2007,9,20");
+// console.log(birthday);
+let today = new Date();
+console.log(today.getFullYear());
+console.log(today.getMonth());
+console.log(today.getDay());
+console.log(today.getHours());
+console.log(today.getMinutes());
+console.log(today.getMilliseconds());
+console.log(today.getSeconds());
