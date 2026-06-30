0d67d300c2cfa4f3891de9e0745ae18c04bcfa87
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-06-30 07:11:43 +0500

    arrays

---
+++ javaScript/Arrays/array.js
@@ -0,0 +1,73 @@
+// Arrays   consists same data type or different data type
+/*
+let arr = [90,75,80,70,68];
+console.log(arr);
+const stuName = ["Uzair", "Ali", "Zubair","Ahmad"];
+console.log(stuName);
+let arr2 = ["Shahid","Umar",89,true];
+console.log(arr2); */
+ 
+ /*     // print array values 
+let marks = [89,75,91,84,60,74,86];
+
+//using for loop
+
+// for (i = 0; i<marks.length; i++) {
+//     console.log(marks[i]);
+// }
+
+//using for-in loop (best and modern way)
+
+// for (let mark of marks) {
+//     console.log(mark);
+// }
+//prints value by index
+//   console.log(marks[0]);
+//   console.log(marks[1]);
+//   console.log(marks[3]);
+//   console.log(marks[5]);
+
+// change value by index
+// marks[3]=88;
+// console.log(marks);              */
+
+let furits = ["mango","orange","banana","watermellon"];
+// push() insert element in the last
+// furits.push("apple");
+//console.log(furits);
+//unshift()  add element in the starting
+// furits.unshift("litchi",8,);
+// console.log(furits);
+ // pop() delement from the end  shift() delete from starting
+//  furits.pop();
+//  furits.shift();
+//  console.log(furits);
+// slice  return a new array no change in orignal
+// a=furits.slice(1,3);
+// console.log(a)
+//splice modify a oraginals arrays
+//  furits.splice(1,2,"apple");
+// console.log(furits);
+
+//sort
+
+furits.sort();
+console.log(furits);
+
+let score = [78,101,2000,93,86,35,];  //this is sorting as a strings only sort first number comparing
+/*
+num.sort();
+console.log(num);
+num.sort((a,b) => a-b) // ascending sort
+console.log(score); */
+
+// toString
+// a=score.toString();
+// console.log(a);
+
+//console.log(score.indexOf(86));
+//console.log(furits.concat(score));
+// console.log(score.join("-"));
+//console.log(score.reverse());
+
+
