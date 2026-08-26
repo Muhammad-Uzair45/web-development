e2d949084bc42a21e60dd796134fbdd778c2a821
Muhammad Uzair <muhammad.uzair4522@gmail.com>
2026-08-26 10:33:04 +0500

    promise learn

--- javaScript/callbackHell-promise-asyn-await/first.js
+++ javaScript/callbackHell-promise-asyn-await/first.js
@@ -27,7 +27,7 @@ console.log(result);
 
 
 // callback Hell
-
+/*
 function getdata(dataId,nextdata){
     setTimeout( ()=>{
         console.log("data  =" ,dataId);
@@ -49,4 +49,52 @@ function getdata(dataId,nextdata){
       });  
     });
  });
+         */
+//promise  
+   /*
+const p1 = new Promise((resolve, reject) => {
+    console.log("hello");
+    resolve("success");
+
+});
+//console.log(p1)
+p1.then( (data)=> {
+    console.log(data);
+});            */
+
+/*
+const promise = new Promise((resolve, reject) => {
+
+    resolve("Success");
+});
+promise.then((result) => {
+        console.log(result);
+    })
+       .catch((error) => {
+        console.log(error);
+    })
+      .finally(() => {
+        console.log("Operation finished");
+    });
+   // console.log(promise);    */
+
+   const p2 = new Promise( (resolve,reject) => {
+   // console.log("1st");
+   
+    setTimeout( () => {
+        resolve("Data received");
+    }, 3000);
+
+   });
+   
+   //console.log("2nd");
+
+   p2.then( (res) => {
+    console.log(res);
+   });
+    //console.log("3rd");
+
+
+
+
 
